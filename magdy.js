4a3f9c05ecfa877cd8a20e355dff4b64ff4f6a9a
Mahmoud Magdy <mahmoudmagdy603@gmail.com>
2020-02-03 23:08:11 +0200

    first commit

---
+++ magdy.js
@@ -0,0 +1,53 @@
+class Duck{      //class for creating ducks
+    constructor(duckColor){
+        this.duckColor=duckColor;
+        switch(this.duckColor)
+        {
+            case "red":
+                this.imageSrc="gallery/1.jpg" //"gallery/redDuck.png";
+                break;
+            case "black":
+                this.imageSrc="gallery/blackDuck.png";
+                break;
+            case "gold":
+                this.imageSrc="gallery/goldenDuck.png";
+                break;      
+        }
+    }
+    createDuck(){
+        let duckObject=$(`<span class="Duck"><img src=${this.imageSrc}></span>`);
+        return duckObject;
+    }
+}
+class Dog{   //class for creating the dog
+    createDog(){
+        let dogObject=$(`<span class="Dog"><img src="gallery/dog.png"></span>`);
+        return dogObject; 
+    }
+}
+class Bomb{   //class for creating the bomb
+    createBomb(){
+        let bombObject=$(`<span class="Bomb"><img src="gallery/Bomb.png"></span>`);
+    }
+}
+class DiedDuck{  //class for creating died ducks
+    constructor(duckColor){
+        this.diedDuckColor=duckColor;
+    switch(this.diedDuckColor)
+        {
+            case "red":
+                this.imageSrc="gallery/1.jpg";
+                break;
+            case "black":
+                this.imageSrc="gallery/blackDuck.png";
+                break;
+            case "gold":
+                this.imageSrc="gallery/goldenDuck.png";
+                break;      
+        }
+    }
+    createDiedDuck(){
+        let diedDuck=$(`<span class="DiedDuck"><img src=${this.imageSrc}></span>`);
+        return diedDuck;
+    }    
+}
